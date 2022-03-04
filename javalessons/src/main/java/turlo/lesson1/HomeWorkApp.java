bfa1193526009b91d0e1cf9f009eebdded6257f3
НовыйЯ <turlotv@mail.ru>
2022-03-04 09:07:52 +0300

    Homework

--- javalessons/src/main/java/turlo/lesson1/HomeWorkApp.java
+++ javalessons/src/main/java/turlo/lesson1/HomeWorkApp.java
@@ -25,10 +25,10 @@ public class HomeWorkApp {
                 System.out.println("Сумма отрицательная");
             }}
          public static void printColor() {
-                int value = 200;
-                if (value <= 0) {
-                    System.out.println("Красный");
-                }
+             int value = 200;
+             if (value <= 0) {
+                 System.out.println("Красный");
+             }
                 if (value > 0 && value <= 100) {
                     System.out.println("Жёлтый");
                 }
