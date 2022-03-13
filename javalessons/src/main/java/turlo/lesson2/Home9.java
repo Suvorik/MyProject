8fcd37d049a7d2c551235984c3821fe0d3d531a1
НовыйЯ <turlotv@mail.ru>
2022-03-13 11:50:44 +0300

    lesson3 done2

--- javalessons/src/main/java/turlo/lesson2/Home9.java
+++ javalessons/src/main/java/turlo/lesson2/Home9.java
@@ -2,20 +2,23 @@ package turlo.lesson2;
 
 public class Home9 {
     public static void main(String[] args) {
-        arr(5,5);
-        printArrayInConsole(int[] inputArray;)
+        arr(5, 5);
     }
-    public static int[] arr(int len,int initialValue){
-        int[] arr =new int[len];
-        for(int i=0; i<arr.length;i++){
-            arr[i]=initialValue;
+
+    public static int[] arr(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = initialValue;
         }
         return arr;
     }
-    public static void printArrayInConsole(int[] inputArray){
-        for(int i=0; i<inputArray.length;i++){
 
-    System.out.print(inputArray[i] +" ");}
+    public static void printArrayInConsole(int[] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+
+            System.out.print(inputArray[i] + " ");
+        }
         System.out.println();
-        printArrayInConsole(arr(5,5));
-}}
+        printArrayInConsole(arr(5, 5));
+    }
+}
